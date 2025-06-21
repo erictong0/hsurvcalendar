f60b9fba365c3f66459090ab659bb883e36597bd
Eric Tong <erictong@college.harvard.edu>
2025-06-21 16:21:53 -0400

    Base React website

---
+++ README.old.md
@@ -0,0 +1,2 @@
+# HSURVCalendar
+GCal doesn't allow for images, so this website aims to create a central space for all community-run events for the 2025 HSURV Program and display them in a clear manner
---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="bg-green-200 text-center p-4 rounded shadow">
+        ✅ Tailwind is working!
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
